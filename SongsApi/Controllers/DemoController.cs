5e7346cf9cd7f0b799b2ba36e1cb1011db19efd5
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-24 13:54:44 -0400

    Add project files.

---
+++ SongsApi/Controllers/DemoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongsApi.Controllers
+{
+    public class DemoController : ControllerBase
+    {
+        // GET /status
+        [HttpGet("/status")]
+        public ActionResult<GetStatusResponse> GetTheStatus()
+        {
+
+            var response = new GetStatusResponse
+            {
+                Message = "Everything is operational.",
+                LastChecked = DateTime.Now
+            };
+            return Ok(response);
+        }
+        // GET /products/38938983983 (Route Param)
+        [HttpGet("/products/{productId:int}")] // Route Constraints.
+        public ActionResult LookupProduct(int productId)
+        {
+            return Ok("Information about product: " + productId);
+        }
+    }
+
+    public class GetStatusResponse
+    {
+        public string Message { get; set; }
+        public DateTime LastChecked { get; set; }
+    }
+}
