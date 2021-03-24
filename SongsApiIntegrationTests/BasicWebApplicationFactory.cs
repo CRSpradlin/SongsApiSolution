0011ffa59cc7bb66f688c5a1e72bc4a3958b24e9
Christopher Spradlin <github@crspradlin.org>
2021-03-24 15:46:39 -0400

    Added integration test project to solution

---
+++ SongsApiIntegrationTests/BasicWebApplicationFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using SongsAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongsApiIntegrationTests
+{
+    // This runs your test on a faked hosted api, hense why we passed in Startup
+    public class BasicWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+
+    }
+}
