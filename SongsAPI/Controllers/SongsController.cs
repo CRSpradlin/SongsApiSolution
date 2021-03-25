dfb28989f1c384833d28c285d0b49e48b015ca2c
Christopher Spradlin <github@crspradlin.org>
2021-03-25 16:10:37 -0400

    Added post request

--- SongsAPI/Controllers/SongsController.cs
+++ SongsAPI/Controllers/SongsController.cs
@@ -18,6 +18,43 @@ namespace SongsAPI.Controllers
             _context = context;
         }
 
+        [HttpPost("/songs")]
+        public async Task<ActionResult> AddASong([FromBody] PostSongRequest request)
+        {
+            // 1. Validate the Entity (Title is Required, but Artist isn't)
+            //      - If not valid, send a 400 with or without some details about what they did wrong.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            // 2. Modify the domain - save it to the database.
+            var song = new Song
+            {
+                Title = request.Title,
+                Artist = request.Artist,
+                RecommendedBy = request.RecommendedBy,
+                IsActive = true,
+                AddedToInventory = DateTime.Now
+            };
+            _context.Songs.Add(song);
+            //Can add multiple songs here
+            await _context.SaveChangesAsync();
+            // 3. Return:
+            //      - 201 Created Status Code
+            //      - Give them a copy of the newly created resource.
+            //      - Add a location header with the URL of the newly created resource. -- For caching purposes
+            //          E.g. Location: http://localhost:1337/songs/5
+            // Fluent Validation can help with automatic API validation.
+            var response = new GetASongResponse
+            {
+                Id = song.Id,
+                Title = song.Title,
+                Artist = song.Artist,
+                RecommendedBy = song.RecommendedBy
+            };
+            return CreatedAtRoute("songs#getasong", new { id = response.Id }, response);
+        }
+
         [HttpGet("/songs")]
         public async Task<ActionResult> GetAllSongs()
         {
@@ -41,6 +78,29 @@ namespace SongsAPI.Controllers
 
 
             return Ok(response);
+        }
+
+        [HttpGet("/songs/{id:int}", Name = "songs#getasong")]
+        public async Task<ActionResult> GetASong(int id)
+        {
+            var response = await _context.Songs
+                .Where(s => s.IsActive && s.Id == id)
+                .Select(s => new GetASongResponse
+                {
+                    Id = s.Id,
+                    Title = s.Title,
+                    Artist = s.Artist,
+                    RecommendedBy = s.RecommendedBy
+                })
+                .SingleOrDefaultAsync();
+
+            if(response == null)
+            {
+                return NotFound();
+            } else
+            {
+                return Ok(response);
+            }
         }
     }
 }
