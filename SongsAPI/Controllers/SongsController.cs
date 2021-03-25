9713c127545347e1c95bca15a6a7bee51d7b6fd7
Christopher Spradlin <github@crspradlin.org>
2021-03-25 12:06:15 -0400

    added controller to return correct json data structure

--- SongsAPI/Controllers/SongsController.cs
+++ SongsAPI/Controllers/SongsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SongsAPI.Domain;
+using SongsAPI.Models.Songs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,10 +21,24 @@ namespace SongsAPI.Controllers
         [HttpGet("/songs")]
         public async Task<ActionResult> GetAllSongs()
         {
-            var response = await _context.Songs
+            var response = new GetSongsResponse();
+
+            var data = await _context.Songs
                 .Where(song => song.IsActive == true)
+                .Select(song => new SongSummaryItem //Created SongSummaryItem for every song to create the correct data type to form
+                {
+                    Id = song.Id,
+                    Title = song.Title,
+                    Artist = song.Artist,
+                    RecommendedBy = song.RecommendedBy
+                })
                 .OrderBy(song => song.Title)
                 .ToListAsync();
+
+            //Can't just do this without having the .Select with the query above
+            response.Data = data;
+
+
 
             return Ok(response);
         }
---
+++ SongsAPI/Models/Songs/GetSongsResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongsAPI.Models.Songs
+{
+    //Created this representation in JSON first, pasted it as JSON and edited some uppercasings and changed the array of data to a List for ease of use
+    public class GetSongsResponse
+    {
+        public List<SongSummaryItem> Data { get; set; }
+    }
+
+    public class SongSummaryItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string RecommendedBy { get; set; }
+    }
+}
